1a3ef5b4d045c6bf2ecd395a11ea2f1806473e45
Manal <manal.alghanmi@gmail.com>
2021-04-15 22:51:11 +0300

    expanding the output display to show all columns of DataFrame

--- bikeshare.py
+++ bikeshare.py
@@ -1,6 +1,7 @@
 import time
 import pandas as pd
 import numpy as np
+pd.set_option('display.max_columns',200)
 
 CITY_DATA = { 'chicago': 'chicago.csv',
               'new york city': 'new_york_city.csv',
